54c198fd836776e0ec147016c9dc512c7c5457ff
Justin Mekenye Opini <opinijm@clarkson.edu>
2019-12-08 21:16:10 -0500

    Update CaesarDecipher.cpp
    Functional refactor for healthier code

--- CaesarDecipher.cpp
+++ CaesarDecipher.cpp
@@ -1,148 +1,191 @@
-//Purpose: Deecipher Caesar Cipher via letter frrequency analysis
-//Developed by: Justin Opini(opinijm@clarkson.edu)
-//Last Modified : 11/06/2017
+/**
+ * @author Justin Opini(opinijm@clarkson.edu)
+ * Last Modified : 12/07/2019
+ * Deciphers Caesar input via letter frequency analysis
+ */
+
+#include<algorithm>
 #include<iostream>
 #include<fstream>
-#include<algorithm>
-#include<string>
-#include<cstdlib>
-//###################################################################### GLOBAL VARIABLES ########################################################################################
-const int alphabetsSIZE = 26;
-int sortedFrequencies[alphabetsSIZE];
-struct alphabetInfo{
-    char alphabet;int frequency;
-    int rank;char decodedAlphabet;int shift;
-};
-//################################################################################################################################################################################
-int rank(int);
-void sortFreq(int[alphabetsSIZE]);
-void write(std::string,std::string);
-int main() {
-    //######################################################## VARIABLES #######################################################################
-    char alphabets[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-    char sortedAlphabets[] = {'j', 'z', 'q', 'k', 'x', 'v', 'g', 'w', 'b', 'p', 'y', 'f', 'c', 'u', 'm', 'd', 'l', 'h', 'o', 'n', 'i', 'r', 's', 'a', 't', 'e'};
-    int alphabetfrequency[alphabetsSIZE] = {0};
-    int shiftValues[alphabetsSIZE] = {0};
-    int modeShift = 0;
-    int buffer = 0;
-    const int ASCIIconvert = 97;
-    char bufferAlphabet;
-    std::string toDecode = "";
-    std::string temp;
-    std::string decoded;
-    std::string original;
-    std::string fileName;
-    long messageLength = 0;
-    alphabetInfo current;
-    std::ifstream cipher;
-    //############################################################################################################################################
-    std::cout << "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^CAESAR CIPHER DECIPHER^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"<< std::endl;
-    std::cout<< " Please enter the file name to be decrypted" << std::endl;
-    std::cin >> fileName;
-    cipher.open( fileName.c_str());
-    if ( cipher.fail()) {
-        std::cout<< "WARNING!! make sure that the filename is correct in the correct case!! and in the same path as the programme" <<std::endl;
-        sleep(2);
-        main();
-    }
-    while (!cipher.eof()) {
-        getline(cipher,temp);
-        toDecode += temp;
-    }
-    original = toDecode;
-    messageLength = toDecode.length();
-    //Read in file
-    for (int a = 0; a < messageLength; ++a)
-    {
-        toDecode[a] = tolower(toDecode[a]);
-        if(ispunct(toDecode[a])) {
-            toDecode[a] = ' ';
-        }
-    }
-    //Frequecy of each letter
-    for (int b = 0; b < messageLength; b++) {
-        buffer = toDecode[b] - 97;
-        if (buffer >= 0) {
-            alphabetfrequency[buffer]++;
-        }
-    }
-    //sort frequencies globally
-    sortFreq(alphabetfrequency);
-    //calculates all letters shift values
-    for(int c = 0; c < alphabetsSIZE; c++){
-        current.alphabet = alphabets[c];
-        current.frequency = alphabetfrequency[c];
-        current.rank = rank(current.frequency);
-        current.decodedAlphabet = sortedAlphabets[current.rank];
-        current.shift = current.decodedAlphabet - current.alphabet;
-        shiftValues[c] = current.shift;
-        }
-    //Finds mode shift value
-    std::sort(shiftValues,shiftValues+alphabetsSIZE);
-    for(int i = 0,highestMode = 0,modeCount = 0,currentmode = shiftValues[0],highestmodecount = 0;i < alphabetsSIZE;i++)
-    {
-        if (shiftValues[i] == currentmode) {
-            modeCount++;
-        } else {
-            if (modeCount > highestmodecount) {
-                highestMode = currentmode;
-                highestmodecount = modeCount;
-                modeCount = 0;
-            }
-        }
-        currentmode = shiftValues[i];
-        modeShift = highestMode;
-    }
-    //decodes message with mode shift value
-    decoded = toDecode;
-    for (int y = 0; y < messageLength; y++){
-            if (isalpha(decoded[y])) {
-                buffer = decoded[y] - ASCIIconvert;
-                buffer = (buffer + modeShift) % alphabetsSIZE;
-                buffer = buffer + ASCIIconvert;
-                bufferAlphabet = buffer;
-                decoded[y] = bufferAlphabet;
-                if(decoded[y] < ASCIIconvert){
-                    buffer = ASCIIconvert - decoded[y];
-                    buffer = (ASCIIconvert+alphabetsSIZE) - buffer;
-                    bufferAlphabet = buffer;
-                    decoded[y] = bufferAlphabet;
-                }
-                //restors capitalization
-                if (isupper(original[y])) {
-                    decoded[y] = toupper(decoded[y]);
-                }
+#include <functional>
+#include <set>
+#include <vector>
 
-            } else{}
-        //restores punctuation
-        if (ispunct(original[y])) {
-                    decoded[y] = original[y];
-                }
-     }
-    std::cout << decoded << std::endl;
-    write(fileName,decoded);
+typedef std::pair<char, int> letter_freq;
+typedef std::function<bool(letter_freq, letter_freq)> comparator;
+
+static const int ALPHABET_SIZE = 26;
+static const int ASCII_ALPHA_LOWER_BOUND = 97;
+static const int ASCII_ALPHA_UPPER_BOUND = ASCII_ALPHA_LOWER_BOUND + ALPHABET_SIZE;
+static const char SORTED_ALPHABETS_BY_NORMAL_FREQ[] = {'j', 'z', 'q', 'k', 'x', 'v', 'g', 'w', 'b', 'p', 'y', 'f', 'c', 'u', 'm', 'd', 'l', 'h', 'o', 'n', 'i', 'r', 's', 'a', 't', 'e'};
+
+static std::string prettify(std::string str);
+static std::set<letter_freq, comparator>get_char_frequencies(std::string str);
+static int get_mode_shift(std::string str);
+static char decode_char(char c, int shift);
+static void write(const std::string &, const std::string &);
+
+int main() {
+    //Get the file name and set up the stream.
+    std::string file_name;
+    std::ifstream input;
+    while (!input.is_open() || input.fail()) {
+        std::cout<< " Please enter a valid file name to be decrypted: ";
+        std::cin >> file_name;
+        input.open( file_name.c_str());
+    }
+
+    //Read the file.
+    std::string file_content((std::istreambuf_iterator<char>(input)), (std::istreambuf_iterator<char>()));
+    input.close();
+
+    int mode_shift = get_mode_shift(file_content);
+
+    //Decodes message with mode shift value.
+    char decoded_letter;
+    int index = 0;
+    for (char c : file_content){
+        if (isalpha(c)) {
+            decoded_letter = decode_char(static_cast<char>(tolower(c)), mode_shift);
+
+            //Restores capitalization.
+            if (isupper(c)) {
+                file_content[index] = static_cast<char>(toupper(decoded_letter));
+            } else{
+                file_content[index] = decoded_letter;
+            }
+
+        }
+        ++index;
+    }
+
+    write(file_name, file_content);
     return 0;
 }
-//sorts frequencies globally
-void sortFreq(int frequencyarray[alphabetsSIZE]) {
-    for (int x = 0; x < alphabetsSIZE; x++) {
-        sortedFrequencies[x] = frequencyarray[x];
+
+/**
+ * Removes punctuations and makes all letters lower case.
+ * @param str
+ * @return 'pretiffied' str
+ */
+std::string static prettify(std::string str){
+    for (char &c : str) {
+        c = static_cast<char>(tolower(c));
+        if(ispunct(c)) {
+            c = ' ';
+        }
     }
-    std::sort(sortedFrequencies,sortedFrequencies+alphabetsSIZE);
+
+    return str;
 }
-//returns the rank of the frequency in ascending order
-int rank(int toInput) {
-    static int found = 0;
-    found = std::distance(sortedFrequencies,std::find(sortedFrequencies,sortedFrequencies+alphabetsSIZE,toInput));
-    return found;
+
+/**
+ *
+ * @param str
+ * @return The most frequent shift based from the normal distribution in English.
+ */
+static int get_mode_shift(std::string str){
+    str = prettify(str);
+    std::set<letter_freq, comparator> sorted_letter_frequencies = get_char_frequencies(str);
+    std::vector<int > shift_values;
+
+    // Calculate each individual shift
+    int freq_rank = 0;
+    int shift;
+    for (letter_freq e : sorted_letter_frequencies) {
+        shift = SORTED_ALPHABETS_BY_NORMAL_FREQ[freq_rank] - e.first;
+        shift_values.push_back(shift);
+        freq_rank += 1;
+    }
+
+
+    //Find mode shift
+    int highest_count, current_count = 0;
+    int mode_shift, current_shift = 0;
+    std::sort(shift_values.begin(), shift_values.end());
+    for(int s : shift_values)
+    {
+        if(s == current_shift){
+            ++current_count;
+        } else{
+            if (current_count > highest_count){
+                mode_shift = s;
+            }
+            current_count = 0;
+            current_shift = s;
+        }
+
+    }
+
+    return mode_shift;
 }
-//Outputs the decoded message to a new file
-void write(std::string fileName, std::string message){
-    std::ofstream decipher;
-    static int loc = fileName.find('.');
-    fileName = fileName.substr(0,loc);
-    fileName += "_decoded.txt";
-    decipher.open(fileName.c_str());
-    decipher << message;
-    std::cout << "\n\nYour message has been 100% decoded to the file: " << fileName << std::endl;
+
+/**
+ * Generates letter frequencies of each letter.
+ * @param str
+ * @return a set of pairs that can be iterated through letter frequency order
+ */
+static std::set<letter_freq, comparator> get_char_frequencies(std::string str) {
+    int letter_frequencies[ALPHABET_SIZE] = {0};
+    int int_char;
+    for (char c: str) {
+        int_char = c - ASCII_ALPHA_LOWER_BOUND;
+        if (int_char >= 0)
+            letter_frequencies[int_char]++;
+    }
+    std::vector<letter_freq> letter_frequencies_pairs;
+    char c = 'a';
+    for (int i: letter_frequencies){
+        letter_frequencies_pairs.emplace_back(c, i);
+        ++c;
+    }
+
+    // Sorts by second element of pair (frequency).
+    const comparator comp =
+            [](letter_freq freq1 ,letter_freq freq2){
+                return freq1.second < freq2.second;
+            };
+
+
+    // Set that is iterable through letter frequency.
+    std::set<letter_freq, comparator> sorted_letter_frequencies(
+            letter_frequencies_pairs.begin(), letter_frequencies_pairs.end(), comp);
+    return sorted_letter_frequencies;
+}
+
+/**
+ *
+ * @param c
+ * @param shift
+ * @return decoded c
+ */
+static char decode_char(char c, int shift){
+    int int_letter = 0;
+    int_letter = c - ASCII_ALPHA_LOWER_BOUND;
+    int_letter = (int_letter + shift) % ALPHABET_SIZE;
+    int_letter += ASCII_ALPHA_LOWER_BOUND;
+
+    // In case shift is negative.
+    if(int_letter < ASCII_ALPHA_LOWER_BOUND){
+        int overflow = ASCII_ALPHA_LOWER_BOUND - int_letter;
+        int_letter = ASCII_ALPHA_UPPER_BOUND - overflow;
+    }
+    return static_cast<char>(int_letter);
+
+}
+
+/**
+ * Outputs the decoded message to a new file
+ * @param file_name
+ * @param decrypted_message
+ */
+static void write(const std::string &file_name, const std::string &decrypted_message){
+    static std::ofstream writer;
+    static unsigned long file_extension_location = file_name.find('.');
+    static std::string output_file_name = file_name.substr(0, file_extension_location) + "_decoded.txt";
+
+    writer.open(output_file_name.c_str());
+    writer << decrypted_message;
+    writer.close();
+    std::cout << "\n\nYour message has been decoded to the file: " << output_file_name << std::endl;
 }
