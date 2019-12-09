1d5920ac72aea8fb743748054190f965536681de
Justin Mekenye Opini <opinijm@clarkson.edu>
2019-12-08 22:53:11 -0500

    Update CaesarDecipher.cpp

--- CaesarDecipher.cpp
+++ CaesarDecipher.cpp
@@ -27,8 +27,8 @@ static void write(const std::string &, const std::string &);
 
 int main() {
     //Get the file name and set up the stream.
-    static std::string file_name;
-    static std::ifstream input;
+    std::string file_name;
+    std::ifstream input;
     while (!input.is_open() || input.fail()) {
         std::cout<< " Please enter a valid file name to be decrypted: ";
         std::cin >> file_name;
@@ -39,11 +39,11 @@ int main() {
     std::string file_content((std::istreambuf_iterator<char>(input)), (std::istreambuf_iterator<char>()));
     input.close();
 
-    static int mode_shift = get_mode_shift(file_content);
+    int mode_shift = get_mode_shift(file_content);
 
     //Decodes message with mode shift value.
-    static char decoded_letter;
-    static int index = 0;
+    char decoded_letter;
+    int index = 0;
     for (char c : file_content){
         if (isalpha(c)) {
             decoded_letter = decode_char(static_cast<char>(tolower(c)), mode_shift);
@@ -74,8 +74,8 @@ static int get_mode_shift(std::string str){
     std::vector<int > shift_values;
 
     // Calculate each individual shift
-    static int freq_rank = 0;
-    static int shift;
+    int freq_rank = 0;
+    int shift;
     for (letter_freq e : sorted_letter_frequencies) {
         shift = SORTED_ALPHABETS_BY_NORMAL_FREQ[freq_rank] - e.first;
         shift_values.push_back(shift);
@@ -84,8 +84,8 @@ static int get_mode_shift(std::string str){
 
 
     //Find mode shift
-    static int highest_count, current_count = 0;
-    static int mode_shift, current_shift = 0;
+    int highest_count, current_count = 0;
+    int mode_shift, current_shift = 0;
     std::sort(shift_values.begin(), shift_values.end());
     for(int s : shift_values)
     {
@@ -127,8 +127,8 @@ std::string static prettify(std::string str){
  * @return a set of pairs that can be iterated through letter frequency order
  */
 static std::set<letter_freq, comparator> get_char_frequencies(std::string str) {
-    static int letter_frequencies[ALPHABET_SIZE] = {0};
-    static int int_char;
+    int letter_frequencies[ALPHABET_SIZE] = {0};
+    int int_char;
     for (char c: str) {
         int_char = c - ASCII_ALPHA_LOWER_BOUND;
         if (int_char >= 0)
@@ -142,7 +142,7 @@ static std::set<letter_freq, comparator> get_char_frequencies(std::string str) {
     }
 
     // Sorts by second element of pair (frequency).
-    const comparator comp =
+    static const comparator comp =
             [](letter_freq freq1 ,letter_freq freq2){
                 return freq1.second < freq2.second;
             };
@@ -161,14 +161,14 @@ static std::set<letter_freq, comparator> get_char_frequencies(std::string str) {
  * @return decoded c
  */
 static char decode_char(char c, int shift){
-    static int int_letter, overflow = 0;
+    int int_letter = 0;
     int_letter = c - ASCII_ALPHA_LOWER_BOUND;
     int_letter = (int_letter + shift) % ALPHABET_SIZE;
     int_letter += ASCII_ALPHA_LOWER_BOUND;
 
     // In case shift is negative.
     if(int_letter < ASCII_ALPHA_LOWER_BOUND){
-        overflow = ASCII_ALPHA_LOWER_BOUND - int_letter;
+        int overflow = ASCII_ALPHA_LOWER_BOUND - int_letter;
         int_letter = ASCII_ALPHA_UPPER_BOUND - overflow;
     }
     return static_cast<char>(int_letter);
@@ -181,9 +181,9 @@ static char decode_char(char c, int shift){
  * @param decrypted_message
  */
 static void write(const std::string &file_name, const std::string &decrypted_message){
-    static std::ofstream writer;
-    static unsigned long file_extension_location = file_name.find('.');
-    static std::string output_file_name = file_name.substr(0, file_extension_location) + "_decoded.txt";
+    std::ofstream writer;
+    unsigned long file_extension_location = file_name.find('.');
+    std::string output_file_name = file_name.substr(0, file_extension_location) + "_decoded.txt";
 
     writer.open(output_file_name.c_str());
     writer << decrypted_message;
